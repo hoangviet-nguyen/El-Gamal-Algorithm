38bf351494c962211d9ca896f283e50a90dbf119
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-05-15 13:59:35 +0200

    implemented randomBigInt

--- src/java/key.generator/KeyGenerator.java
+++ src/java/key.generator/KeyGenerator.java
@@ -1,11 +1,23 @@
 package key.generator;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class KeyGenerator {
+    private Random random = new Random();
 
     public BigInteger generateY1(){
         return null;
+    }
+
+
+    public BigInteger randomBigInt(BigInteger range){
+        BigInteger result;
+        do {
+            result = new BigInteger(range.subtract(new BigInteger("1")).bitLength(), random);
+        } while (result.compareTo(range) > 0);
+
+        return result;
     }
 
 }
