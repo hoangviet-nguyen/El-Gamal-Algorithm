0eb7ba2c50c7b19cb3f32b46b1bf44d92d45572d
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-05-15 13:55:14 +0200

    creates KeyGen Class

---
+++ src/java/key.generator/KeyGenerator.java
@@ -0,0 +1,11 @@
+package key.generator;
+
+import java.math.BigInteger;
+
+public class KeyGenerator {
+
+    public BigInteger generateY1(){
+        return null;
+    }
+
+}
--- src/java/main.java
+++ src/java/main.java
@@ -1,12 +1,15 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 import static key.generator.HexString.HEX_STRING;
 
 public class main {
 
     public static void main(String[] args) {
+        Random random = new Random();
 
         BigInteger test = new BigInteger(HEX_STRING.getHexString(), 16);
         System.out.println(test);
+
     }
 }
