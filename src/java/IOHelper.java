2e9fc5b93083053960b7e194657d237903e47631
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-05-18 16:05:37 +0200

    Implemented IOHelper (like in Huffmann)

---
+++ src/java/IOHelper.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class IOHelper {
+
+    public static String readFile(String file) {
+        String text = "";
+        try {
+            Scanner reader = null;
+            File textFile = new File(file);
+            reader = new Scanner(textFile);
+            while (reader.hasNextLine()) {
+                text = reader.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Fehler beim Lesen der Datei");
+            throw new RuntimeException(e);
+        }
+
+        if (text.isEmpty()) {
+            System.out.println("Die Textdatei ist leer.");
+            return "";
+        }
+        return text;
+    }
+
+    public static void writeFile(String text, String file) {
+        try {
+            java.io.FileWriter myWriter = new java.io.FileWriter(file);
+            myWriter.write(text);
+            myWriter.close();
+        } catch (Exception e) {
+            System.out.println("Fehler beim Schreiben der Datei");
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/java/main.java
+++ src/java/main.java
@@ -1,7 +1,5 @@
 import key.generator.KeyGenerator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -23,44 +21,13 @@ public class main {
             switch (input) {
             case 2 -> { //Schlüsselgenerierung
                 BigInteger[] KeyPairB = generator.generateKeyPair();
-                try (FileWriter wr = new FileWriter("sk.txt");
-                     FileWriter wr2 = new FileWriter("pk.txt")) {
 
-                    wr.write(KeyPairB[0].toString()); // privateKey speichern
-                    wr2.write(KeyPairB[1].toString()); // publicKey speichern
-                    System.out.println("öffentlicher und privater Schlüssel erfolgreich gespeichert");
-
-                } catch (IOException ee) {
-                    System.out.println("Fehler beim Speichern der Datei");
-                    throw new RuntimeException(ee);
-                }
+                IOHelper.writeFile(KeyPairB[0].toString(), "sk.txt");
+                IOHelper.writeFile(KeyPairB[1].toString(), "pk.txt");
             }
             case 3 -> { //Verschlüsselung
-                String publicKey = "";
-                Scanner reader;
-                String toEncrypt = "";
-
-                try { //lesen des publicKeys aus pk.txt
-                    File textFile = new File("pk.txt");
-                    reader = new Scanner(textFile);
-                    while (reader.hasNextLine()) {
-                        publicKey = reader.nextLine();
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.println("fehler beim Lesen der Datei");
-                    throw new RuntimeException(e);
-                }
-
-                try { //lesen des unverschlüsselten text aus text.txt
-                    File textFile = new File("text.txt");
-                    reader = new Scanner(textFile);
-                    while (reader.hasNextLine()) {
-                        toEncrypt = reader.nextLine();
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.println("fehler beim Lesen der Datei");
-                    throw new RuntimeException(e);
-                }
+                String publicKey = IOHelper.readFile("pk.txt");
+                String toEncrypt = IOHelper.readFile("text.txt");
 
                 BigInteger key = new BigInteger(publicKey);
 
@@ -82,63 +49,50 @@ public class main {
             case 4 -> { //Entschlüsselung
                 Scanner reader;
                 List<String> encrypted;
-                String privateKeyString = "";
+                String privateKeyString = IOHelper.readFile("sk.txt");
+                String raw = IOHelper.readFile("chiffre.txt"); //lesen des verschlüsselten Text
                 List<List<String>> characters = new ArrayList<>();
-                try {
 
-                    reader = new Scanner(new File("sk.txt")); //lesen des privateKey
-                    privateKeyString = reader.nextLine();
-                    reader = new Scanner(new File("chiffre.txt")); //lesen des verschlüsselten Text
-                    String raw = reader.nextLine();
-                    encrypted = Arrays.stream(raw.substring(1, raw.length() - 1)
-                            .split(";")) //splitten der (Y1,Y2);(Y1,Y2); zu separaten (Y1,Y2)
-                        .toList();
+                encrypted = Arrays.stream(raw.substring(1, raw.length() - 1)
+                        .split(";")) //splitten der (Y1,Y2);(Y1,Y2); zu separaten (Y1,Y2)
+                    .toList();
 
-                    for (String values : encrypted) {  //splitten der (Y1,Y2) zu werten Y1 und Y2 , entfernung der Klammern
-                        characters.add(Arrays.stream(values.replaceAll("\\(", "").
-                            replaceAll("\\)", "").split(",")).toList());
+                for (String values : encrypted) {  //splitten der (Y1,Y2) zu werten Y1 und Y2 , entfernung der Klammern
+                    characters.add(Arrays.stream(values.replaceAll("\\(", "").
+                        replaceAll("\\)", "").split(",")).toList());
+
+
+                    ArrayList<Character> ausgabe = new ArrayList<>();
+                    BigInteger privateKey = new BigInteger(privateKeyString);
+
+                    for (List<String> values_ : characters) { //entschlüsselung jedes chars
+                        char decrypted =
+                            generator.decrypt(new BigInteger(values_.get(0)), new BigInteger(values_.get(1)),
+                                privateKey);
+                        System.out.println(decrypted);
+                        ausgabe.add(decrypted);
                     }
-
-                } catch (FileNotFoundException e) {
-                    System.out.println("fehler beim Lesen der Datei");
-                    throw new RuntimeException(e);
-                }
-
-                ArrayList<Character> ausgabe = new ArrayList<>();
-                BigInteger privateKey = new BigInteger(privateKeyString);
-
-                for (List<String> values : characters) { //entschlüsselung jedes chars
-                    char decrypted =
-                        generator.decrypt(new BigInteger(values.get(0)), new BigInteger(values.get(1)), privateKey);
-                    System.out.println(decrypted);
-                    ausgabe.add(decrypted);
-                }
-
-
-                try {  //speichern des entschlüsselten textes
-                    FileWriter wr = new FileWriter(("text-d.txt"));
+                    String output = "";
                     for (Character character : ausgabe) {
-                        wr.write(character);
+                        output += character;
                     }
-                    wr.flush();
-                    wr.close();
-                    System.out.println("Dateien wurde erfolgreich entschlüsselt");
 
-                } catch (IOException ee) {
-                    System.out.println("fehler beim speichern der Datei");
-                    throw new RuntimeException(ee);
+                    IOHelper.writeFile(output, "text-d.txt");
+                    System.out.println("output: " + output);
+                    System.out.println("erfolgreich entschlüsselt");
                 }
             }
             case 5 -> System.out.println("Auf wiedersehen");
 
             default -> System.out.println("ungültige Auswahl");
             }
-        } while (input != 5);
+        }
+        while (input != 5);
 
     }
 
     private static int menu() {
-        //menü concept aus der ersten Programmieraufgabe übernommen
+        //menü Konzept aus der ersten Programmieraufgabe übernommen
         scanner = new Scanner(System.in);
         System.out.println("Bitte wählen Sie eine Option aus:");
         System.out.print(" 2. Schlüsselgenerierung |");
